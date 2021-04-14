419b931d6e9f775c71d5e19f9f0371fd920de096
Saurabh <modi.2@iitj.ac.in>
2021-04-14 20:47:44 +0530

    break

--- polls/urls.py
+++ polls/urls.py
@@ -13,10 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+We are right now using function views here.
+1. Empty url is never finished by '/'
+
+2. Always a good habit to end the urlpattern(except empty url) with '/'
+   It will make it to --> http://127.0.0.1:8000/polls/3/
+   
+   when user gives --> http://127.0.0.1:8000/polls/3 or http://127.0.0.1:8000/polls/3/
+
+3. If you do not do then url will only run when user uses --> http://127.0.0.1:8000/polls/3
+   It will fail if user writes --> http://127.0.0.1:8000/polls/3/
+   
+4. Another UrlConfigration means url of other app as it url change
+  Ex: if you are in mysite app then your current location is http://127.0.0.1:8000/mysite.urls
+  but you want to access urls of polls app then its address is http://127.0.0.1:8000/polls.urls
+  thus you will need to use 'include' so that the address changes from 
+  http://127.0.0.1:8000/mysite.urls --> http://127.0.0.1:8000/polls.urls
+"""
+
 from django.urls import path
 from polls import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index', views.index, name='index')
+    path('<int:question_id>/', views.detail, name='details'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,27 @@
+from django.http.response import Http404
 from django.shortcuts import render,HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello you are at polls app home page")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    #     context = {'question':question}
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = 
+    return render(request, 'polls/detail.html', context)
+
+def results(request, question_id):
+    return HttpResponse("you are looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you are voting on the question %s." % question_id) 
